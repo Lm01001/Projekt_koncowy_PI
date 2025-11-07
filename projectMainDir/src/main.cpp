ad8ae1a586c8373cdfed3619fb49a659695f232f
Lm01001 <maksymilian.kaczmarczyk@interia.pl>
2025-11-07 14:43:19 +0200

    test window + lanuch vscode settings

--- projectMainDir/src/main.cpp
+++ projectMainDir/src/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
+
 using namespace std;
+//g++ ../../src/main.cpp -lsfml-graphics -lsfml-window -lsfml-system -o run
 
 int main() {
+	sf::RenderWindow window(sf::VideoMode(800,600), "Test window");
+	while(window.isOpen()){
+		sf::Event event;
+		while(window.pollEvent(event)){
+			if(event.type == sf::Event::Closed)
+				window.close();
+		}
+		window.clear();
+		window.display();
+	}
 
 	return 0;
 }
