0ba462eee8bc8e99b60c59f5c8b88dcf5d320b92
Lm01001 <maksymilian.kaczmarczyk@interia.pl>
2025-11-12 10:32:12 +0200

    tworzenie przycisku - potrzeba naprawy bledow, analizy przy budowie

--- projectMainDir/src/main.cpp
+++ projectMainDir/src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
+#include <bits/stdc++.h>
+
 #include "SnakeDetails.h"
 #include "SnakeGUI.h"
 
@@ -9,7 +11,8 @@ using namespace std;
 
 int main() {
 	SnakeGui instForGui = SnakeGui();
-	//SnakeDetails snakeDetailsInstance = SnakeDetails();
+	SnakeGui buttonObj = SnakeGui();
+	sf::Vector2i pozycjaMyszy;
 
 	/*
 	* Zmienna typu RenderWindow - wyswietlane okno, RectangleShape pod stworzenie przycisku
@@ -17,8 +20,10 @@ int main() {
 	sf::RenderWindow window(sf::VideoMode(550, 650), "Gra Snake");
 	//sf::RectangleShape gornyPasekOkna = instForGui.stworzPasekOkna(800, 50, sf::Color(168, 168, 168));
 	sf::RectangleShape mainButtonFrame = instForGui.stworzPrzycisk(175, 400, 200, 100, sf::Color(89,43,66));
-	sf::RectangleShape mainButton = instForGui.stworzPrzycisk(190, 415, 170, 70, sf::Color(192, 192, 192));
-	
+	sf::RectangleShape mainButton = buttonObj.stworzPrzycisk(190, 415, 170, 70, sf::Color(192, 192, 192));
+	buttonObj.xWspolrzedna = 190;
+	buttonObj.yWspolrzedna = 415;
+
 	/*
 	*
 	*/
@@ -29,14 +34,31 @@ int main() {
 
 	while(window.isOpen()){
 		sf::Event event;
+		mainButton.setFillColor(sf::Color(192, 192, 192));
+		button.isMouseOn = false;
+
 		while(window.pollEvent(event)){
 			if(event.type == sf::Event::Closed)
 				window.close();
 		}
-		//window.clear();
+
+		pozycjaMyszy = Mouse::getPosition(window);
+		if(pozycjaMyszy.x <= buttonObj.xWspolrzedna && pozycjaMyszy <= buttonObj.yWspolrzedna &&
+			pozycjaMyszy.x >= buttonObj.xWspolrzedna + 170 && pozycjaMyszy.y >= buttonObj.yWspolrzedna + 70){
+				button.isMouseOn = true;
+		}
+		window.clear();
 		instForGui.ustawTloOkna(window, sf::Color(222, 216, 194));
 		window.draw(mainButtonFrame);
-		window.draw(mainButton);
+
+		if(!button.isClicked){
+			mainButton.setPosition(sf::Vector2f(button.xWspolrzedna, button.yWspolrzedna));
+			window.draw(mainButton);
+		}
+		if(button.isClicked){
+			mainButton.setFillColor(sf::Color(128,128,128));
+		}
+		
 		window.display();
 	}
 
