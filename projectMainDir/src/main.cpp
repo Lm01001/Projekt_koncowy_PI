2decb48acda669e70779ddf7b1ed8a059fef0d9f
Lm01001 <maksymilian.kaczmarczyk@interia.pl>
2025-11-12 13:15:55 +0200

    Dodane repozytorium do tworzenia przyciskow

--- projectMainDir/src/main.cpp
+++ projectMainDir/src/main.cpp
@@ -5,58 +5,74 @@
 
 #include "SnakeDetails.h"
 #include "SnakeGUI.h"
+#include "Button.hpp"
+#include "RectButton.hpp"
 
 using namespace std;
-//g++ ../../src/main.cpp -lsfml-graphics -lsfml-window -lsfml-system -o run
+using namespace sf; 
+
+//g++ src/*.cpp -I include -lsfml-graphics -lsfml-window -lsfml-system -o run
 
 int main() {
 	SnakeGui instForGui = SnakeGui();
 	SnakeGui buttonObj = SnakeGui();
-	sf::Vector2i pozycjaMyszy;
+	RectButton button = RectButton(Vector2f(170,70), Vector2f(190, 415));
+	Vector2i pozycjaMyszy;
 
+
+	button.setButtonLabel(10, "abcde");
+	//button.setLabelColor(Color(89,43,66));
 	/*
 	* Zmienna typu RenderWindow - wyswietlane okno, RectangleShape pod stworzenie przycisku
 	*/
-	sf::RenderWindow window(sf::VideoMode(550, 650), "Gra Snake");
+	RenderWindow window(VideoMode(550, 650), "Gra Snake", Style::Default);
+	window.display();
+
+	VideoMode desktopScreen = VideoMode::getDesktopMode();
+	int x = (desktopScreen.width - 800) / 2;
+	int y = (desktopScreen.height - 800) / 2;
+	window.setPosition(sf::Vector2i(x, y));
+	window.setVerticalSyncEnabled(true);
+	
 	//sf::RectangleShape gornyPasekOkna = instForGui.stworzPasekOkna(800, 50, sf::Color(168, 168, 168));
-	sf::RectangleShape mainButtonFrame = instForGui.stworzPrzycisk(175, 400, 200, 100, sf::Color(89,43,66));
-	sf::RectangleShape mainButton = buttonObj.stworzPrzycisk(190, 415, 170, 70, sf::Color(192, 192, 192));
+	RectangleShape mainButtonFrame = instForGui.stworzPrzycisk(175, 400, 200, 100, Color(89,43,66));
+	RectangleShape mainButton = buttonObj.stworzPrzycisk(190, 415, 170, 70, Color(192, 192, 192));
 	buttonObj.xWspolrzedna = 190;
 	buttonObj.yWspolrzedna = 415;
 
 	/*
 	*
 	*/
-	sf::VideoMode desktopScreen = sf::VideoMode::getDesktopMode();
-	int x = (desktopScreen.width - 800) / 2;
-	int y = (desktopScreen.height - 800) / 2;
-	window.setPosition(sf::Vector2i(x, y));
 
+	//Sprite sprite;
+	//sprite.setPosition(0, 0);
 	while(window.isOpen()){
-		sf::Event event;
-		mainButton.setFillColor(sf::Color(192, 192, 192));
-		button.isMouseOn = false;
+		Event event;
+		mainButton.setFillColor(Color(192, 192, 192));
+		buttonObj.isMouseOn = false;
 
 		while(window.pollEvent(event)){
-			if(event.type == sf::Event::Closed)
+			if(event.type == Event::Closed)
 				window.close();
 		}
 
 		pozycjaMyszy = Mouse::getPosition(window);
-		if(pozycjaMyszy.x <= buttonObj.xWspolrzedna && pozycjaMyszy <= buttonObj.yWspolrzedna &&
+		if(pozycjaMyszy.x <= buttonObj.xWspolrzedna && pozycjaMyszy.y <= buttonObj.yWspolrzedna &&
 			pozycjaMyszy.x >= buttonObj.xWspolrzedna + 170 && pozycjaMyszy.y >= buttonObj.yWspolrzedna + 70){
-				button.isMouseOn = true;
+				buttonObj.isMouseOn = true;
 		}
 		window.clear();
-		instForGui.ustawTloOkna(window, sf::Color(222, 216, 194));
+		//window.draw(sprite);
+		instForGui.ustawTloOkna(window, Color(222, 216, 194));
 		window.draw(mainButtonFrame);
+		button.draw(window);
 
-		if(!button.isClicked){
-			mainButton.setPosition(sf::Vector2f(button.xWspolrzedna, button.yWspolrzedna));
+		if(!buttonObj.isClicked){
+			mainButton.setPosition(Vector2f(buttonObj.xWspolrzedna, buttonObj.yWspolrzedna));
 			window.draw(mainButton);
 		}
-		if(button.isClicked){
-			mainButton.setFillColor(sf::Color(128,128,128));
+		if(buttonObj.isClicked){
+			mainButton.setFillColor(Color(128,128,128));
 		}
 		
 		window.display();
