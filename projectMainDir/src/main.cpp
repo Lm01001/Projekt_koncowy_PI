81cb5da27d448b638161d2b876657906d76f10cb
Lm01001 <maksymilian.kaczmarczyk@interia.pl>
2025-11-13 00:22:41 +0200

    Zmieniono solo SFML na TGUI. 1. Uproszczenie i zwiekszenie wygody tworzenia gui + mniej kodu. 2. Dokończony CMakeLists i budowanie projektu bez pisania dlugiej komendy. 3. Budowanie + uruchomienie: w /build najpierw make => ./projektGra  4. Do zrobienia moze klasa pod stricte szybsze ustawienia wlasnosci przycisku

--- projectMainDir/src/main.cpp
+++ projectMainDir/src/main.cpp
@@ -3,91 +3,84 @@
 #include <SFML/Window.hpp>
 #include <bits/stdc++.h>
 
+#include <TGUI/TGUI.hpp>
+#include <TGUI/Backend/SFML-Graphics.hpp>
+
 #include "SnakeDetails.h"
 #include "SnakeGUI.h"
 
 using namespace std;
-using namespace sf; 
+using namespace sf;
 
 //g++ src/*.cpp -I include -lsfml-graphics -lsfml-window -lsfml-system -o run 
+//git submodule add https://github.com/texus/TGUI.git projectMainDir/libs/SFML-Button
 
 int main() {
 	SnakeGui instForGui = SnakeGui();
 	SnakeGui buttonObj = SnakeGui();
-	Vector2i pozycjaMyszy;
 
 	/*
 	* Zmienna typu RenderWindow - wyswietlane okno, RectangleShape pod stworzenie przycisku
 	*/
 	RenderWindow window(VideoMode(550, 650), "Gra Snake", Style::Default);
-	window.display();
 
 	VideoMode desktopScreen = VideoMode::getDesktopMode();
-	int x = (desktopScreen.width - 800) / 2;
-	int y = (desktopScreen.height - 800) / 2;
+	int x = (desktopScreen.width - 550) / 2;
+	int y = (desktopScreen.height - 650) / 2;
 	window.setPosition(sf::Vector2i(x, y));
 	window.setVerticalSyncEnabled(true);
 	
+
+	tgui::Gui gui{window};
+	/*
+	*
+	*	wrzucic to jako funkcje do klasy ustawianie tego wszystkiego
+	*
+	*/
+	auto mainButton = tgui::Button::create("Graj");
+	mainButton->setPosition(190, 435);
+	mainButton->setSize(170, 70);
+	mainButton->getRenderer()->setBackgroundColor(tgui::Color(192, 192, 192));
+	mainButton->getRenderer()->setBackgroundColorHover(tgui::Color(200, 200, 200));
+	mainButton->getRenderer()->setBorderColor(tgui::Color(89, 43, 66));
+	mainButton->getRenderer()->setBorders(5);
+	mainButton->setTextSize(28);
+
+	mainButton->onPress([]() {
+		cout<<"dziala przycisk"<<endl;
+	});
 	//sf::RectangleShape gornyPasekOkna = instForGui.stworzPasekOkna(800, 50, sf::Color(168, 168, 168));
-	RectangleShape mainButtonFrame = instForGui.stworzPrzycisk(175, 400, 200, 100, Color(89,43,66));
+	/*RectangleShape mainButtonFrame = instForGui.stworzPrzycisk(175, 400, 200, 100, Color(89,43,66));
 	RectangleShape mainButton = buttonObj.stworzPrzycisk(190, 415, 170, 70, Color(192, 192, 192));
 	buttonObj.xWspolrzedna = x + 190;
 	buttonObj.yWspolrzedna = y + 415;
 
 	/*
 	*
-	*/
+	
 	const string text = "Graj";
 	Text text1;
 	text1.setString(text);
 	text1.setPosition(Vector2f(buttonObj.xWspolrzedna + 30,  buttonObj.yWspolrzedna + 30));
 	text1.setFillColor(Color::Black);
-	text1.setCharacterSize(30);
+	text1.setCharacterSize(30);*/
+
+
+	gui.add(mainButton);
+
 	while(window.isOpen()){
 		Event event;
-		mainButton.setOutlineColor(Color(89,43,66));
-		mainButton.setOutlineThickness(5);
-		mainButton.setFillColor(Color(192, 192, 192));
-		buttonObj.isMouseOn = false;
-
 		while(window.pollEvent(event)){
+			gui.handleEvent(event);
+
 			if(event.type == Event::Closed)
 				window.close();
-		}
-
-		pozycjaMyszy = Mouse::getPosition();
-		if(pozycjaMyszy.x >= buttonObj.xWspolrzedna && pozycjaMyszy.y >= buttonObj.yWspolrzedna &&
-			(pozycjaMyszy.x <= buttonObj.xWspolrzedna + 170) && (pozycjaMyszy.y <= buttonObj.yWspolrzedna + 70) &&
-			Mouse::isButtonPressed(Mouse::Left)){
-				buttonObj.isMouseOn = true;
-				//window.close();				
-		}
-	
-		window.clear();
-		instForGui.ustawTloOkna(window, Color(222, 216, 194));
-		window.draw(mainButton);
-		/*if(buttonObj.isMouseOn){
-			mainButton.setPosition(Vector2f(buttonObj.xWspolrzedna, buttonObj.yWspolrzedna));
-			window.draw(mainButton);
-		}*/
-		
-
-		/*
-		*	dla pauzy
-		*/
-		if(Keyboard::isKeyPressed(Keyboard::Escape)){
+			if(event.type == Event::KeyPressed && event.key.code == Keyboard::Escape)
 				window.close();
 		}
-		
 
-		/*if(!buttonObj.isClicked){
-			mainButton.setPosition(Vector2f(buttonObj.xWspolrzedna, buttonObj.yWspolrzedna));
-			//window.draw(mainButton);
-		}
-		if(buttonObj.isClicked){
-			mainButton.setFillColor(Color(128,128,128));
-		}*/
-
+		window.clear(Color(222, 216, 194));
+		gui.draw();
 
 		window.display();
 	}
